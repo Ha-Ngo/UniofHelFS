7c96b1f33460ec0a5bba6949fcf4ec6faa161c79
hango <hangolethanh@gmail.com>
2021-04-09 11:22:20 +0300

    done 2-6

--- Part 2/courseinfo/src/components/Total.js
+++ Part 2/courseinfo/src/components/Total.js
@@ -4,13 +4,14 @@ export const Total = (props) => {
   return (
     <div>
       <strong>
-      Total of{" "}
-      {props.total
-        .map((part) => part.exercises)
-        .reduce((total, curVal) => {
-          return total + curVal;
-        })} exercises
-        </strong>
+        Total of{" "}
+        {props.total
+          .map((part) => part.exercises)
+          .reduce((total, curVal) => {
+            return total + curVal;
+          })}{" "}
+        exercises
+      </strong>
     </div>
   );
 };
