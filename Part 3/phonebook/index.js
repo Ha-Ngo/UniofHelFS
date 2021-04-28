0ee6078f707a5b75201ed2d418b6e5e8345f3f6f
hango <hangolethanh@gmail.com>
2021-04-28 11:52:08 +0300

    done 3-12

--- Part 3/phonebook/index.js
+++ Part 3/phonebook/index.js
@@ -65,7 +65,6 @@ app.get("/api/persons/:id", (request, response) => {
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((person) => persons.id !== id);
-
   response.status(204).end();
 });
 
